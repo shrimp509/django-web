b96b88dff86eaa1c7a85a28d9ed9e6fcc8019ab2
RongSon <shrimp509@hotmail.com>
2020-02-11 10:28:18 +0800

    Add static files of css

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.http import HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.core.handlers.wsgi import WSGIRequest
 import json
 
 from .forms import PostForm
 from .models import Post
+
 
 def post_create_view(request):
 	form = PostForm(request.POST or None)
@@ -27,13 +27,9 @@ def test_json_response_view(request: WSGIRequest):
 		print("get GET request: ", request)
 		return JsonResponse({'first': 'content', 'second': 'test'})
 	elif request.method == 'POST':
-		# get data
+		# get json data
 		data = json.loads(request.body)
 		print("get POST request data: ", data)
-
-		# turn to Post Object
-		post = Post(title=data['title'], content=data['content'])
-		print("post: ", post)
 
 		# save to db
 		Post.objects.create(title=data['title'], content=data['content'])
@@ -47,7 +43,6 @@ def test_json_response_view(request: WSGIRequest):
 		return JsonResponse({'status': 'no, I don\'t know it.'})
 
 
-# Create your views here.
 def hello(request):
 	return render(request, 'blog/index.html', {
 			"first_var": "Hello Man",
