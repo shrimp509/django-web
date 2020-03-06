5ec7b37d33f276655942d2089041d580d22e5a99
RongSon <shrimp509@hotmail.com>
2020-03-06 15:15:59 +0800

    Fix main.py: wrong package name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from django_shop_web.wsgi import application
+from mysite.wsgi import application
 
 # App Engine by default looks for a main.py file at the root of the app
 # directory with a WSGI-compatible object called app.
